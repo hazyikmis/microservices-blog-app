d7eaa217d129c96b5d152b5dd48c072aad4c308f
Halil Azyikmis <hazyi@halo-pc-hp.hacettepe.edu.tr>
2020-07-09 01:41:24 +0200

    event bus data store added and implemented

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,8 +6,12 @@ const axios = require("axios");
 const app = new express();
 app.use(bodyParser.json());
 
+const events = []; //event bus data store
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event); //ordered list of all events from beginning up to now
 
   //we are assuming that all posts below successfully sent
   //normally there must be some kind of error handling mechanism
@@ -18,6 +22,10 @@ app.post("/events", (req, res) => {
 
   res.send({status: "OK"});
 });
+
+app.get("/events", (req, res) => {
+  res.send(events);
+})
 
 app.listen(4005, () => {
   console.log("Event bus service is listening on port: 4005")
